b002b180722259cbf8799a3e0747095e75129ca7
= <parenoj@oregonstate.edu>
2017-01-26 20:57:41 -0800

    added file parsing for items, variable length list

--- justin/loadRoom.cpp
+++ justin/loadRoom.cpp
@@ -8,23 +8,28 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using std::string;
 using std::cout;
 using std::cin;
 using std::endl;
+using std::vector;
 
 void loadRoom(string filename)
 {
     string roomName, longDesc, shortDesc;
-    bool isVisited, northLocked, southLock, westLocked, eastLocked;
-    //Have to figure out items, because there can be a variable amount
+    bool isVisited, northLocked, southLocked, westLocked, eastLocked;
+    //Have to figure out items, because there can be a variable amount -- Vector, duh
+	vector<string> items;
+	int i = 0;
 
     string buffer;
 
     std::ifstream roomFile (filename);
     if(roomFile.is_open())
     {
+		//These first calls to getline are standard, they will always be in every room file
         getline(roomFile, buffer);
         roomName = buffer;
 
@@ -35,9 +40,35 @@ void loadRoom(string filename)
         shortDesc = buffer;
 
         getline(roomFile, buffer);
-        isVisited = std::stoi(buffer, nullptr);
+        isVisited = std::stoi(buffer);
+		
+		getline(roomFile, buffer);
+        northLocked = std::stoi(buffer);
+		
+		getline(roomFile, buffer);
+        southLocked = std::stoi(buffer);
+		
+		getline(roomFile, buffer);
+        westLocked = std::stoi(buffer);
+		
+		getline(roomFile, buffer);
+        eastLocked = std::stoi(buffer);
+		
+		//Now, we get to the items, which can vary in number so we loop
+		while(getline(roomFile, buffer))
+		{
+			items.push_back(buffer);
+			i++;
+		}
+		
+		
     }
-    cout << roomName << endl << longDesc << endl << shortDesc << endl;
+    cout << roomName << endl << longDesc << endl << shortDesc << endl << isVisited << endl << northLocked << ',' << southLocked << ',' << westLocked << ',' << eastLocked << endl;
+	
+	for(int j = 0; j < i; j++)
+	{
+		cout << items[j] << endl;		
+	}
 }
 
 int main()
